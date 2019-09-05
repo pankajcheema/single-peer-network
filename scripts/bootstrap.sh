1561b27be9406a99feefd60e7eb6732b8c410670
pankaj cheema <pankajcheema1990@gmail.com>
2019-09-05 16:17:10 +0530

    add and update document guide updated

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -22,10 +22,6 @@ peer chaincode instantiate -o orderer.example.com:7050 --tls --cafile /opt/gopat
 
 #peer chaincode instantiate -o orderer.example.com:7050 --tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem -C $CHANNEL_NAME -n singleledger -v 1.0 -c '{"Args":[]}' -P "AND ('Org1MSP.peer')" --collections-config $GOPATH/src/github.com/chaincode/singleledger/Chaincodes/SingleLedgerConnectorV1/collections_config.json
 
-sleep 10
-
-peer chaincode invoke -o orderer.example.com:7050 --tls true --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem -C $CHANNEL_NAME -n mycc --peerAddresses peer0.org1.example.com:7051 --tlsRootCertFiles /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt -c '{"Args":["addPacket","{\"name\": \"pankaj\",\"description\": \"agsdhagsdhgasjdhgasjdgas\",\"deadline\": \"24\/08\/2019\",\"created_by\": \"pamkaj\"}"]}' 
-
 
 
 #peer chaincode invoke -o orderer.example.com:7050 --tls true --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem -C $CHANNEL_NAME -n singleledger-chain --peerAddresses peer0.org1.example.com:7051 --tlsRootCertFiles /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt -c '{"Args":["RegisterCompany","{\"docType\":\"Corporation\",\"Organization\":\"SlickPie\",\"StructureType\":\"Corporation\",\"Name\":\"CompanyABCD\",\"CompanyEmail\":\"info@companyabcd.com\",\"LegalName\":\"Technical Company ABCD\",\"Website\":\"www.companyabcd.com\",\"Sector\":\"Food,service, aerospace\",\"RegisterCode\":\"349803264328943\",\"CompanyBlockChainID\":\"ahdjkhfkhfkj43hkhkshdkjsahkj23khkjh435h\",\"CompanyId\":12}"]}' --transient "{\"company\":\"$JSONPVT\"}"
